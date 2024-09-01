26f938192e7e86cda1fc4652caa69cbeb059d1e5
John Paul <johnpaulrayco1@gmail.com>
2024-09-01 15:09:52 +0800

    added search functionality to the movie website

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,20 @@ const API_URL = 'http://www.omdbapi.com?apikey=f894c396';
 
 function App() {
 
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      document.getElementById('js-button-search').click();
+    }
+    
+  })
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
+    setMovies(data.Search);
     console.log(data.Search);
   }
 
@@ -26,7 +36,7 @@ function App() {
     "imdbID": "tt0372784",
     "Type": "movie",
     "Poster": "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
-}
+  } 
 
   return (
     <>
@@ -34,14 +44,22 @@ function App() {
         <Logo></Logo>
         <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
           <input type="text" placeholder='Search a movie' style={{height: "47px", width: "500px", fontSize: "20px", paddingInlineStart: "20px", borderRadius: "10px 0 0 10px"}}
-          
-          onChange={(e) => {}}
+          value={searchTerm}
+          onChange={(e) => {setSearchTerm(e.target.value)}}
           />
-          <button style={{height: "50px", width: "50px", borderRadius: "0px 10px 10px 0px", backgroundColor: "#fe7b00"}}
-          onClick={(e) => {}}
-          ><i class="ri-search-line" style={{fontSize: "27px", fontWeight: "600"}}></i></button>
+          <button className='button-search' id='js-button-search' style={{height: "50px", width: "50px", borderRadius: "0px 10px 10px 0px", backgroundColor: "#fe7b00"}}
+          onClick={() => searchMovies(searchTerm)}
+          ><i className="ri-search-line" style={{fontSize: "27px", fontWeight: "600"}}></i></button>
         </div>
-        <Movies movie={movie1}></Movies>
+
+        <div className='container-movies'>
+          {
+            movies?.length > 0 ? movies.map((movie) => <Movies moviename={movie}/>)
+            : 
+            (<div className='empty'><h2 style={{color: "#ffffff"}}>NO MOVIES FOUND!</h2></div>) 
+          }
+        </div>
+        
       </div> 
     </>
   );
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,21 +1,22 @@
+import '../styles/movieCard.css';
 
-export const Movies = (movies) => {
+export const Movies = ({ moviename }) => {
 
-    const {movie} = movies;
+    
     
     return (
         <>
             <div className="container">
                 <div className="movie">
                     <div>
-                        <p>{movie.Year}</p>
+                        <p>{moviename.Year}</p>
                     </div>
                     <div>
-                        <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title} />
+                        <img src={moviename.Poster !== 'N/A' ? moviename.Poster : 'https://via.placeholder.com/400'} alt={moviename.Title} />
                     </div>
                     <div>
-                        <span>{movie.Type}</span>
-                        <h3>{movie.Title}</h3>
+                        <span>{moviename.Type}</span>
+                        <h3>{moviename.Title}</h3>
                     </div>
                 </div>
             </div>
